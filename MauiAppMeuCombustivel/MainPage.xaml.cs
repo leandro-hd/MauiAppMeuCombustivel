cba835893d5b69ad9fff97cb60f7aa6623d380c4
Leandro Illan Higa Dias <leandro.dias@tecnospeed.com.br>
2024-10-12 18:42:20 -0300

    chore: AGENDA-10 - Adicionar campos de marca e modelo do veículo

--- MauiAppMeuCombustivel/MainPage.xaml.cs
+++ MauiAppMeuCombustivel/MainPage.xaml.cs
@@ -13,15 +13,17 @@
             {
                 double etanol = Convert.ToDouble(txt_etanol.Text);
                 double gasolina = Convert.ToDouble(txt_gasolina.Text);
+                string marca_veiculo = txt_marca_veiculo.Text;
+                string modelo_veiculo = txt_modelo_veiculo.Text;
 
                 string msg = "";
 
                 if(etanol <= (gasolina * 0.7) )
                 {
-                    msg = "O etanol está compensando.";
+                    msg = $"O etanol está compensando para o seu {marca_veiculo} {modelo_veiculo}.";
                 } else
                 {
-                    msg = "A gasolina está compensando.";
+                    msg = $"A gasolina está compensando para o seu {marca_veiculo} {modelo_veiculo}.";
                 }
 
                 DisplayAlert("Calculado", msg, "OK");
